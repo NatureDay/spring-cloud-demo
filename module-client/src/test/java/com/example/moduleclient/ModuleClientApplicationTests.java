72e1e818f6622e42aad3b72b7e79bb36d0fd466a
Nature_Day <1004934782@qq.com>
2019-10-15 14:40:05 +0800

    init project

---
+++ module-client/src/test/java/com/example/moduleclient/ModuleClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.moduleclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModuleClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
