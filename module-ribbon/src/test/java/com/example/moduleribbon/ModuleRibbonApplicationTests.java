3fbcd0bb2231ac8945265d0942b479e2ffaebaef
Nature_Day <1004934782@qq.com>
2019-10-15 15:30:46 +0800

    update project

---
+++ module-ribbon/src/test/java/com/example/moduleribbon/ModuleRibbonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.moduleribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModuleRibbonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
