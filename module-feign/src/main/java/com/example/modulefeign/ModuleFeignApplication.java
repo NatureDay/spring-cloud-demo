1550fed3d3e3f6e0276e5e382fe06873f8b94ead
Nature_Day <1004934782@qq.com>
2019-10-15 16:16:07 +0800

    update project

---
+++ module-feign/src/main/java/com/example/modulefeign/ModuleFeignApplication.java
@@ -0,0 +1,13 @@
+package com.example.modulefeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleFeignApplication.class, args);
+    }
+
+}
