8d3b377d0cc7baa16a92ccf732b541b9ccf5667c
Nature_Day <1004934782@qq.com>
2019-10-16 11:16:48 +0800

    update project

--- module-ribbon/src/main/java/com/example/moduleribbon/HelloService.java
+++ module-ribbon/src/main/java/com/example/moduleribbon/HelloService.java
@@ -1,5 +1,6 @@
 package com.example.moduleribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +11,13 @@ public class HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "defaltError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://module-client/hi?name=" + name, String.class);
+    }
+
+    public String defaltError(String name) {
+        return "hi," + name + ",sorry,error!";
     }
 
 }
