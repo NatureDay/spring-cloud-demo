ada4889a03e252dfcab51bb411cb7317d3c630d3
Nature_Day <1004934782@qq.com>
2019-10-16 14:38:04 +0800

    update project

--- module-feign/src/main/java/com/example/modulefeign/ModuleFeignApplication.java
+++ module-feign/src/main/java/com/example/modulefeign/ModuleFeignApplication.java
@@ -1,9 +1,17 @@
 package com.example.modulefeign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
+/**
+ * 开启hystrixDashboard
+ */
+@EnableHystrixDashboard
 @EnableFeignClients
 @SpringBootApplication
 public class ModuleFeignApplication {
@@ -12,4 +20,19 @@ public class ModuleFeignApplication {
         SpringApplication.run(ModuleFeignApplication.class, args);
     }
 
+    /**
+     * Spring Cloud Finchley 版本以前访问路径是/hystrix.stream，如果是Finchley的话就需要加入上面的配置。
+     * 因为spring Boot 2.0.x以后的actuator只暴露了info和health2个端点
+     *
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
